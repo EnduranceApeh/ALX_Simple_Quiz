8c6ff9d68495d7accb138d0fa6d584cc146b5843
EnduranceApeh <apehendurance2@gmail.com>
2025-11-28 14:15:40 +0100

    Implement a Simple Interactive Quiz

---
+++ quiz.js
@@ -0,0 +1,19 @@
+function checkAnswer() {
+  const correctAnswer = '4';
+
+  const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+  const feedback = document.getElementById('feedback');
+
+  if(userAnswer === correctAnswer) {
+    feedback.textContent = 'Correct! Well done.'
+    console.log('correct')
+  } else if(userAnswer !== correctAnswer) {
+    feedback.textContent = "That's incorrect. Try again!"
+  }
+
+}
+
+const submitBtn = document.getElementById('submit-answer');
+
+submitBtn.addEventListener('click', checkAnswer)
+
